90afccdb34770aca5c72e9ca5bc34615127a784c
Smart Ted <fedor-commander@yandex.ru>
2018-12-25 16:38:01 +0400

    парлалала 3

--- MISHEN.py
+++ MISHEN.py
@@ -7,8 +7,22 @@ screen = pygame.display.set_mode(size)
 
 for i in range(0, k):
     Rect = ((i*n, i*n), ((k-i)*2*n, (k-i)*2*n))
+    if k >= 3:
+        if i % 3 == 2:
+            color = (255, 0, 0)
+        if i % 3 == 1:
+            color = (0, 255, 0)
+        if i % 3 == 0:
+            color = (0, 0, 255)
+    elif k == 2:
+        if i == 0:
+            color = (0, 255, 0)
+        else:
+            color = (255, 0, 0)
+    else:
+        color = (255, 0, 0)
 
-    pygame.draw.ellipse(screen, color1 , Rect, n)
+    pygame.draw.ellipse(screen, color, Rect, n)
 
 pygame.display.flip()
 
