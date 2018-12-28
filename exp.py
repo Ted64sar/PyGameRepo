c6a625b6088ab30644658c2477c57f09b78430f6
Ted64sar <fedor-commander@yandex.ru>
2018-12-28 22:31:39 +0400

    мой коммит

---
+++ exp.py
@@ -0,0 +1,38 @@
+import pygame
+
+class Board:
+    # создание поля
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+        self.board = [[0] * width for _ in range(height)]
+        # значения по умолчанию
+        self.left = 10
+        self.top = 10
+        self.cell_size = 30
+
+    # настройка внешнего вида
+    def set_view(self, left, top, cell_size):
+        self.left = left
+        self.top = top
+        self.cell_size = cell_size
+
+    def get_click(self, mouse_pos):
+        cell = self.get_cell(mouse_pos)
+        self.on_click(self, cell)
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            board.get_click(event.pos)
+
+board = Board(4, 3)
+board.set_view(100, 100, 50)
+pygame.init()
+screen = pygame.display.set_mode((400, 400))
+board = Board(5, 7)
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+    screen.fill((0, 0, 0))
+    board.render()
+    pygame.display.flip()
