16e4d8dcf0a9ceeddeb362c46343c98f1e795165
Ted64sar <fedor-commander@yandex.ru>
2019-01-27 14:17:56 +0400

    мой коммит

--- BOOM_THEM_ALL.py
+++ BOOM_THEM_ALL.py
@@ -1,1 +1,18 @@
 import pygame
+
+
+class Bomb(pygame.sprite.Sprite):
+    image = load_image("bomb.png")
+
+    def __init__(self, group):
+        # НЕОБХОДИМО вызвать конструктор родительского класса Sprite.
+        # Это очень важно!!!
+        super().__init__(group)
+        self.image = Bomb.image
+        self.rect = self.image.get_rect()
+        self.rect.x = random.randrange(width)
+        self.rect.y = random.randrange(height)
+
+    def update(self):
+        self.rect = self.rect.move(random.randrange(3) - 1,
+                                   random.randrange(3) - 1)
